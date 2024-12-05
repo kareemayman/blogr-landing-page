769f4f540a0c0ce5530ea3e96c04e970fd41701c
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-05 16:31:06 +0200

    Created Vite App & Imported CSS Files

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import '../src/css/normalize.css'
+import '../src/css/index.css'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(
+    <App></App>
+)
