001104ee1b6565a1c48a4489d8dccb125846cbe9
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-10 16:19:44 +0200

    Added useEffect() To HeaderLink Component To Handle User Clicks & Header Link Menus Properly

--- src/Components/HeaderLink.jsx
+++ src/Components/HeaderLink.jsx
@@ -1,11 +1,45 @@
+import { useEffect } from "react"
+
 export function HeaderLink({ links, children }) {
-  function handleClick(e) {
-    const span = e.target.querySelector("span") || e.target.closest("span")
-    span.classList.toggle("visibility")
-  }
+  // useEffect to Add Event Listener To Handle Clicking On Header Links
+  useEffect(() => {
+    // Add Function For Click Event
+    const clickEvent = (e) => {
+      // Select All Header Link Menus
+      const spans = document.querySelectorAll("p.header-link span")
+
+      // Handle Clicking On Header Link
+      if (e.target.matches("p.header-link")) {
+        // Select Header Link Menu
+        const span = e.target.querySelector("span")
+
+        // Hide Header Link Menu If It Was Visible
+        if (span.classList.contains("visibility")) {
+          span.classList.remove("visibility")
+        } else {
+          // Hide All Other Header Link Menus & Showing Menu Related To Header Link Clicked
+          spans.forEach((span) => {
+            span.classList.remove("visibility")
+          })
+          span.classList.add("visibility")
+        }
+        // If User Clicks On Any Thing Else Hide Visibile Header Link Menus
+      } else {
+        spans.forEach((span) => {
+          span.classList.remove("visibility")
+        })
+      }
+    }
+    document.addEventListener("click", clickEvent)
+
+    // To Remove Event Listener When Unmounting Component To Avoid Bugs
+    return () => {
+      document.removeEventListener("click", clickEvent)
+    }
+  }, []) // Empty Dependency Array To Run Only 1 Time When Component Is Mounted
 
   return (
-    <p className="header-link ubuntu desktop" onClick={(e) => handleClick(e)}>
+    <p className="header-link ubuntu desktop">
       {children}
       <span className="special-menu">
         {links.map((link) => {
