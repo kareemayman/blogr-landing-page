2857da38edd2bfd3d94614b30561f7b572f2b00c
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-12-11 12:55:04 +0200

    Edited Features Component to Take Props For its Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { Features } from "./Sections/Features"
 import { Hero } from "./Sections/Hero"
 import { StateOfTheArt } from "./Sections/StateOfTheArt"
+import illustrationDesktop from "../images/illustration-editor-desktop.svg"
+import illustrationMobile from "../images/illustration-editor-mobile.svg"
 
 function App() {
   return (
     <>
       <Hero></Hero>
-      <Features></Features>
+      <Features
+        title="Designed for the future"
+        imgDesk={illustrationDesktop}
+        imgMob={illustrationMobile}
+        art1Title="Introducing an extensible editor"
+        art1Parag="Blogr features an exceedingly intuitive interface which lets you
+            focus on one thing: creating content. The editor supports management
+            of multiple blogs and allows easy manipulation of embeds such as
+            images, videos, and Markdown. Extensibility with plugins and themes
+            provide easy ways to add functionality or change the looks of a
+            blog."
+        art2Title="Robust content management"
+        art2Parag="Flexible content management enables users to easily move through
+            posts. Increase the usability of your blog by adding customized
+            categories, sections, format, or flow. With this functionality,
+            you're in full control."
+      ></Features>
       <StateOfTheArt></StateOfTheArt>
     </>
   )
--- src/Sections/Features.jsx
+++ src/Sections/Features.jsx
@@ -1,29 +1,15 @@
-import illustrationDesktop from "../../images/illustration-editor-desktop.svg"
-import illustrationMobile from "../../images/illustration-editor-mobile.svg"
 import { Article } from "../Components/Article"
 
-export function Features() {
+export function Features({ title = '', imgDesk, imgMob, art1Title, art1Parag, art2Title, art2Parag }) {
   return (
     <div className="features overpass" id="features">
       <div className="container">
-        <h1>Designed for the future</h1>
-        <img src={illustrationDesktop} alt="editor illustration" className="desktop"/>
-        <img src={illustrationMobile} alt="editor illustration" className="mobile"/>
+        {title && <h1>{title}</h1>}
+        <img src={imgDesk} alt={imgDesk} className="desktop"/>
+        <img src={imgMob} alt={imgMob} className="mobile"/>
         <div className="article-container">
-          <Article title="Introducing an extensible editor">
-            Blogr features an exceedingly intuitive interface which lets you
-            focus on one thing: creating content. The editor supports management
-            of multiple blogs and allows easy manipulation of embeds such as
-            images, videos, and Markdown. Extensibility with plugins and themes
-            provide easy ways to add functionality or change the looks of a
-            blog.
-          </Article>
-          <Article title="Robust content management">
-            Flexible content management enables users to easily move through
-            posts. Increase the usability of your blog by adding customized
-            categories, sections, format, or flow. With this functionality,
-            you're in full control.
-          </Article>
+          <Article title={art1Title}>{art1Parag}</Article>
+          <Article title={art2Title}>{art2Parag}</Article>
         </div>
       </div>
     </div>
